4c2029ec419bac7aa1df6b1564247233ba6b6c97
pawan <thakunipawan@gmail.com>
2024-09-21 01:57:26 +0530

    commit

--- frontend/src/component/Browse.jsx
+++ frontend/src/component/Browse.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Navbar from './shared/Navbar'
+import LatestJobCards from './LatestJobCards'
+
+const randomJobs=[1,2,3]
 
 const Browse = () => {
   return (
-    <div>Browse</div>
+    <div>
+    <Navbar></Navbar>
+     <div className='max-w-7xl ma-auto mt-5'>
+     <h1 className='font-bold text-xl'>Search Result {randomJobs.length}</h1>
+     <div className='grid grid-cols-3 gap-4 mt-5'>
+     
+     {
+        //instead of <LatestJobCards in need to put <Job> over here later will change it.
+        randomJobs.map((item,index)=><LatestJobCards></LatestJobCards>)
+      }
+     </div>
+     </div>
+    </div>
   )
 }
 
--- frontend/src/component/Jobs.jsx
+++ frontend/src/component/Jobs.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import Navbar from './shared/Navbar'
+import {FilterCard} from './FilterCard.jsx'
+import { Job } from './Job'
+
+const jobArray=[1,2,3,4,5,6,7,8]
+
 
 export const Jobs = () => {
   return (
-    <div>Jobs</div>
+    <div>
+    <Navbar></Navbar>
+    <div className='max-w-7xl mx-auto mt-5'>
+    <div className='flex gap-5'>
+    <div className='w-20%'>
+    <FilterCard></FilterCard>
+    </div>
+    {
+        jobArray.length <=0 ? <span>Job not found</span> :(
+            <div className='flex-1 h-[88vh] overflow-y-auto pb-5'>
+            <div className='grid grid-col-3 gap-4'>
+            { jobArray.map((item,index)=>(<div><Job></Job>
+            </div>
+        ))
+    }
+            </div>
+            </div>
+       
+
+        )
+    }
+    </div>
+    </div><div></div>
+    
+
+   
+    </div>
   )
 }
